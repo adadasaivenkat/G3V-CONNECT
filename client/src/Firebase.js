a31545b0a51af7dc6c683c0f05c888fc7859c136
saivenkatadada <adadasaivenkat0109@gmail.com>
2025-05-17 11:43:46 +0530

    Update Firebase.js

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -7,13 +7,13 @@ import {
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWhmI6Fww8QzraTOxAp3mmDZKmSdG_Er4",
-  authDomain: "g3v-connect.firebaseapp.com",
-  projectId: "g3v-connect",
-  storageBucket: "login-auth-2e9b9.appspot.com",
-  messagingSenderId: "158759198133",
-  appId: "1:158759198133:web:dd3f5787ba9c4a22c68dfc",
-  measurementId: "G-65HQXF8HY2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
