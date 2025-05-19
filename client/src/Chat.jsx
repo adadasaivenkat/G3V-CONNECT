4c27572e560dac1faa760810ce898b649cd9a108
saivenkatadada <adadasaivenkat0109@gmail.com>
2025-05-19 14:26:08 +0530

    Update Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -5,11 +5,34 @@ import { useAuth } from "./context/AuthProvider";
 
 export const Chat = () => {
   const { selectedChat } = useAuth();
-  return (
-    <div className="flex h-[100vh] text-white overflow-hidden">
-      <ContactsCont></ContactsCont>
-      <ChatCont selectedChat={selectedChat} />
 
+  return (
+    <div className="flex h-screen overflow-hidden bg-gray-100 dark:bg-gray-900 relative">
+      {/* Contacts panel */}
+      <div
+        className={`absolute md:static top-0 left-0 h-full w-full
+          md:w-[35vw] lg:w-[30vw] xl:w-[20vw]
+          bg-white dark:bg-gray-800 z-20
+          ${
+            selectedChat
+              ? "translate-x-[-100%] md:translate-x-0"
+              : "translate-x-0"
+          }`}
+        style={{ transition: "none" }}>
+        <ContactsCont />
+      </div>
+
+      {/* Chat panel */}
+      <div
+        className={`absolute md:static top-0 right-0 h-full w-full
+          md:w-[calc(100vw-35vw)] lg:w-[calc(100vw-30vw)] xl:w-[calc(100vw-20vw)]
+          bg-gray-50 dark:bg-gray-900 z-10
+          ${
+            selectedChat ? "translate-x-0" : "translate-x-full md:translate-x-0"
+          }`}
+        style={{ transition: "none" }}>
+        <ChatCont selectedChat={selectedChat} />
+      </div>
     </div>
   );
 };
